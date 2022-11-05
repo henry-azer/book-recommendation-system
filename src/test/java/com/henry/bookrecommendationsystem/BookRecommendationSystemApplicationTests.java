14280ddbe92c06ca02f64ddc7d50035b24bf1fcd
Henry-Azer <henryazer@outlook.com>
2022-11-05 16:57:26 +0200

    [IMPLEMENT] base testing

---
+++ src/test/java/com/henry/bookrecommendationsystem/BookRecommendationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henry.bookrecommendationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRecommendationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
