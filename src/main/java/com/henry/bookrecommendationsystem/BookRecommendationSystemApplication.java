847eda5cfc0fa94d49ed680cfd4b2bb8b85ffaba
Henry-Azer <henryazer@outlook.com>
2022-11-05 17:04:57 +0200

    [IMPLEMENT] book recommendation system application

---
+++ src/main/java/com/henry/bookrecommendationsystem/BookRecommendationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.henry.bookrecommendationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRecommendationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookRecommendationSystemApplication.class, args);
+    }
+
+}
